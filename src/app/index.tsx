93b61c83a36b6d5ac84ea2cdac19a0f2bf0509fb
Miguel Ángel Sánchez Fernández <mangel.sanfer@gmail.com>
2022-07-19 22:33:09 +0200

    Add CRA boilerplate

---
+++ src/app/index.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { Helmet } from 'react-helmet-async';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from 'styles/global-styles';
+import { HomePage } from './pages/HomePage/Loadable';
+import { NotFoundPage } from './components/NotFoundPage/Loadable';
+import { useTranslation } from 'react-i18next';
+
+export function App() {
+  const { i18n } = useTranslation();
+  return (
+    <BrowserRouter>
+      <Helmet
+        titleTemplate="%s"
+        defaultTitle=""
+        htmlAttributes={{ lang: i18n.language }}
+      >
+        <meta name="description" content="" />
+      </Helmet>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+      <GlobalStyle />
+    </BrowserRouter>
+  );
+}
---
+++ src/app/pages/HomePage/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+export function HomePage() {
+  return (
+    <>
+      <Helmet>
+        <title>HomePage</title>
+        <meta name="description" content="" />
+      </Helmet>
+      <span>Hello world!</span>
+    </>
+  );
+}
